24e5e4687d73d5b4f47d176f962a3e34f5cf60bb
Duc111095 <101342718+Duc111095@users.noreply.github.com>
2024-11-04 14:00:06 +0700

    fixed bug

---
+++ main.py
@@ -0,0 +1,23 @@
+import logging
+from connect_db import connect
+
+def insert_db():
+    try:
+        logging.basicConfig(
+            filename="app.log",
+            encoding="utf-8",
+            filemode="a",
+            format="{asctime} : {name} - {levelname} - {message}",
+            style="{",
+            datefmt="%Y-%m-%d %H:%M",
+        )
+        conn = connect.connect_db()
+        cursor = conn.cursor()
+        sql_query = '''
+            insert into zullip_notify(to_person, group_yn, status, datetime0, gc_td1) 
+            select 64, 0, 0, getdate(), 'SET NOCOUNT ON\n{CALL [192.168.100.53\sql2014].MinhAn_App.dbo.GETDL_TBMT_Zulip}'
+        '''
+        cursor.execute(sql_query)
+        logging.info(f'Inserted successfully!')
+    except Exception as e:
+        logging.error(f'{e}')
