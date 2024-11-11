2f76676309da400f3a109448131646822c3bcd82
Duc111095 <101342718+Duc111095@users.noreply.github.com>
2024-11-11 17:05:29 +0700

    fixed bugs

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ def insert_db():
         conn = connect.connect_db()
         cursor = conn.cursor()
         sql_query = """
-            insert into notify_zullip(to_person, group_yn, status, datetime0, gc_td1)
+            insert into notify_zulip(to_person, group_yn, status, datetime0, gc_td1)
             select 64, 0, 0, getdate(), 'SET NOCOUNT ON\n
             {CALL [192.168.100.53\sql2014].MinhAn_App.dbo.GETDL_TBMT_Zulip}'
         """
